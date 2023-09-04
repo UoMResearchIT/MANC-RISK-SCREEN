62402e3143184746e00f52765a281e06c4f20cb1
Martin HA <martin.herrerias@gmail.com>
2023-09-04 12:56:06 +0100

    Add UOM banner and branding

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,3 +1,15 @@
+
+# Custom bootstrap theme
+custom_theme <- bslib::bs_theme(
+  version = 5,
+  bg = "#FFFFFF",
+  fg = "#000000",
+  primary = "#660099",
+  secondary = "#999999",
+  # accent = "#FFCC33"
+  base_font = bslib::font_google("Open Sans")
+)
+
 #' The application User-Interface
 #'
 #' @param request Internal parameter for `{shiny}`.
@@ -15,11 +27,49 @@ app_ui <- function(request) {
     # Leave this function for adding external resources
     golem_add_external_resources(app_title),
 
-    navbarPage(title = app_title,
-               id = "main_tab",
-               selected = "About",
-               fluid = FALSE,
-               collapsible = TRUE,
+    # Banner
+    tag('header', list(
+      id = "banner",
+      class = "header",
+      a(
+        class = "header-item",
+        href = "https://sites.manchester.ac.uk/health-economics/",
+        itemtype = "https://schema.org/Organization",
+        img(
+          src = "img/logo_col_white_background.png",
+          alt = "University of Manchester logo",
+          id = "uom-logo",
+          href = "https://sites.manchester.ac.uk/health-economics/"
+        )
+      ),
+      div(
+        id = "header-title",
+        class = "header-item",
+        p(id = "department", "Manchester Centre for Health Economics"),
+        p(id = "header-appname", app_title)
+      ),
+      div(
+        id = "github-logo-container",
+        class = "header-item",
+        a(
+          href = "https://github.com/UoMResearchIT/MANC-RISK-SCREEN",
+          img(
+            src = "img/github-mark.png",
+            alt = "GitHub logo",
+            id = "github-logo",
+            href = "https://sites.manchester.ac.uk/health-economics/"
+          )
+      ))
+    )),
+
+    bslib::page_navbar(
+      id = "main_tab",
+      selected = "About",
+      fluid = FALSE,
+      collapsible = TRUE,
+      theme = custom_theme,
+      lang = "en",
+      bg = "#660099",
 
       tabPanel("About",
         h1( paste(app_title, "-", packageVersion(pkg)) ),
@@ -33,9 +83,11 @@ app_ui <- function(request) {
         fluidPage( sidebarLayout(
           sidebarPanel( width = 5,
 
-            fluidRow(style = 'margin-right: 3%;',
-                     align = 'right',
+            fluidRow(#style = 'margin-right: 3%;',
+                     #align = 'right',
+                     id = "mod_save_load_reset_ui",
                      mod_save_load_reset_ui("menu")
+
             ),
             br(),
 
@@ -75,6 +127,17 @@ app_ui <- function(request) {
   )
 }
 
+# CSS cache bust
+css_cache_bust <- function(css_file) {
+
+  css_file_copy <- list.files(app_sys("app/www"), paste0(css_file,"#.*"), full.names = TRUE)
+  file.remove(css_file_copy)
+  css_file_copy <- paste0(css_file, "#", format(Sys.time(), "%Y%m%d%H%M%S"))
+  file.copy(css_file, css_file_copy)
+
+  file.path("www",basename(css_file_copy))
+}
+
 #' Add external Resources to the Application
 #'
 #' This function is internally used to add external
@@ -84,18 +147,20 @@ app_ui <- function(request) {
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
 golem_add_external_resources <- function(app_title) {
-  add_resource_path(
-    "www",
-    app_sys("app/www")
-  )
+  add_resource_path("www", app_sys("app/www") )
+  add_resource_path("img", app_sys("app/img") )
+
+  stylesheet <- css_cache_bust("style.css")
 
   tags$head(
     favicon(ext = 'png'),
     bundle_resources(
       path = app_sys("app/www"),
-      app_title = app_title
-    )
+      app_title = app_title,
+      all_files = TRUE
+    ),
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
+    tags$link(rel = "stylesheet", type = "text/css", href = stylesheet)
   )
 }
